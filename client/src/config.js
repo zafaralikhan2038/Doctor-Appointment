72460e3cfe691b739e45a48ccb7b7624a73065fa
zafaralikhan2038 <143221039+zafaralikhan2038@users.noreply.github.com>
2024-09-25 00:48:19 +0530

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,3 @@
-export const BASE_URL = "http://localhost:3600/api/v1";
+export const BASE_URL = "https://doctor-appointment-rlut.onrender.com/api/v1";
+//export const BASE_URL = "http://localhost:3600/api/v1";
 export const token = localStorage.getItem("token");

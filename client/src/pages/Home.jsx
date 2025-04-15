c689bc8552d22a70a6fc394753f5703491413436
zafaralikhan2038 <143221039+zafaralikhan2038@users.noreply.github.com>
2025-04-15 10:30:03 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -33,7 +33,7 @@ const Home = () => {
                   className="text-[36px] leading-[46px] text-headingColor font-[800] md:text-[60px]
                 md:leading-[70px] "
                 >
-                  We help patients live a healthy, longer life.
+                  We help patients live a healthy, and longer life.
                 </h1>
 
                 <p className="text__para">
